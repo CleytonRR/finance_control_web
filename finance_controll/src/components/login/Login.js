51311721706909e7f4cbe8780ef2d07800f1f562
cleytonRR <cleytonrodrigues69@live.com>
2020-02-16 15:30:29 -0300

    Refactor and added api in router

--- finance_controll/src/components/login/Login.js
+++ finance_controll/src/components/login/Login.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import {Link} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
 
+import api from "../../service/api"
 import "./style.css"
 
-export default class Login extends Component {
+class Login extends Component {
+  state = {
+    email: "",
+    password: "",
+    error: ""
+  }
+
+  handleSignUp = async e => {
+    e.preventDefault()
+    const {email, password} = this.state
+    if(!email || !password) {
+      this.setState({error: "Preencha todos os dados para logar"})
+    } else {
+      try {
+        const response = await api.post("/login", {email, password})
+        console.log(response)
+        this.props.history.push("/singUp")
+      } catch (err) {
+        console.log(err)
+        this.setState({error: "Email ou senha invalidos"})
+      }
+    }
+  }
   render() {
     return (
-    <div className="box align-self-lg-center">
+      <div className="box align-self-lg-center">
         <form className="p-3 rounded">
-            <h4 className="text-center text-primary"><i class="fas fa-user-circle"></i></h4>
-            <div className="form-group">
-                <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder="Digite seu email" />
-                <small id="emailHelp" className="form-text">Email único, não compartilhe</small>
-            </div>
+          <h4 className="text-center text-primary"><i class="fas fa-user-circle"></i></h4>
+          {this.state.error && <p className="error text-center">{this.state.error}</p>}
+          <div className="form-group">
+            <input 
+            type="email" 
+            className="form-control" 
+            id="inputEmail" 
+            aria-describedby="emailHelp" 
+            placeholder="Digite seu email" 
+            onChange={e => this.setState({email: e.target.value})}/>
+            <small id="emailHelp" className="form-text">Email único, não compartilhe</small>
+          </div>
 
-            <div className="form-group">
-                <input type="password" className="form-control" id="inputPassword" placeholder="Digite sua senha"/>
-            </div>
-            <button type="submit" className="btn btn-block btn-custom">Sign in</button>
+          <div className="form-group">
+            <input 
+            type="password" 
+            className="form-control" 
+            id="inputPassword" 
+            placeholder="Digite sua senha" 
+            onChange={e => this.setState({password: e.target.value})}/>
+          </div>
+          <button type="submit" className="btn btn-block btn-custom" onClick={this.handleSignUp}>Sign in</button>
         </form>
         <span className="tip">Ainda não tem cadastro?<Link to="/singUp"> Cadastra-se</Link></span>
-    </div>
+      </div>
     )
   }
 }
 
+export default withRouter(Login)

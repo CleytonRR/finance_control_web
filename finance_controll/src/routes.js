ba4ddda4f4f6eab3684cde95a0ff85e3b493a4c4
cleytonRR <cleytonrodrigues69@live.com>
2020-02-16 14:35:04 -0300

    New router

--- finance_controll/src/routes.js
+++ finance_controll/src/routes.js
@@ -1,25 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
     BrowserRouter as Router,
     Switch,
     Route,
+    Redirect,
 } from "react-router-dom"
+import { isAuthenticated } from './service/auth'
 import Login from './components/login/Login'
 import SingUp from './components/singUp/index'
 
 
-export default function Routers() {
-  return (
-    <Router>
-        <Switch>
-            <Route exact path="/">
-                <Login />
-            </Route>
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+        {...rest}
+        render={props => isAuthenticated() ? (
+            <Component {...props} />
+        ): (
+            <Redirect to={{ pathname:"/", state: { from: props.location }}} />
+        )}
+    />
+)
 
-            <Route path="/singUp">
-                <SingUp />
-            </Route>
-        </Switch>
-    </Router>
-  );
+export default function Routers() {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Login />
+                </Route>
+
+                <Route path="/singUp">
+                    <SingUp />
+                </Route>
+
+                <PrivateRoute path="/dashboard" component={() => <h1>Logou com sucesso</h1>}/>
+            </Switch>
+        </Router>
+    );
 }

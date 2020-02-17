31a958006e408c333341c6df970dc002e0663314
cleytonRR <cleytonrodrigues69@live.com>
2020-02-16 21:43:14 -0300

    Configure starting to reactChart2

--- finance_controll/src/components/graph/index.js
+++ finance_controll/src/components/graph/index.js
@@ -1,13 +1,45 @@
-import React, { Component } from "react"
-import api from "../../service/api"
+import React from 'react';
+import {Bar} from 'react-chartjs-2';
 
-
-class Graph extends Component {
-    render() {
-        return (
-            <h1>Nestá sessão você pode visualizar os resultados com base em gráficos</h1>
-        )
+const dados = [65, 59, 80, 81, 56, 55, 40]
+const colors = dados.map((item) => {
+    if(item > 60) {
+        return "#36A2EB"
+    } else {
+        return "#FF6384"
     }
-}
+})
+const data = {
+  labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+  datasets: [
+    {
+      label: 'My First dataset',
+      backgroundColor: 'rgba(255,99,132,0.2)',
+      borderColor: 'rgba(255,99,132,1)',
+      borderWidth: 1,
+      hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+      hoverBorderColor: 'rgba(255,99,132,1)',
+      data: dados,
+      backgroundColor: colors
+    }
+  ]
+};
 
-export default Graph
+export default class Graph extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h2>Bar Example (custom size)</h2>
+        <Bar
+          data={data}
+          width={100}
+          height={50}
+          options={{
+            maintainAspectRatio: false
+          }}
+        />
+      </div>
+    );
+  }
+}

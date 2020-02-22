c1b87ddcb983047b521e194680f95e4ec36a6420
cleytonRR <cleytonrodrigues69@live.com>
2020-02-22 15:10:06 -0300

    Refactor and added button for logout

--- finance_controll/src/pages/dashboard/index.js
+++ finance_controll/src/pages/dashboard/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import CreateCashDay from '../../components/createCashDay/index'
 import Graph from '../../components/graph/index'
+import {logout} from '../../service/auth'
+import {Link} from 'react-router-dom'
 
 import "./style.css"
 
@@ -8,9 +10,14 @@ export default class Dashboard extends Component {
     render() {
         return (
             <div className="container">
+                <div className="d-flex justify-content-end">
+                    <button className="btn btn-warning" onClick={e => logout()}>
+                        <Link to="/"><i class="fas fa-sign-out-alt"></i>Sair</Link>
+                    </button>
+                </div>
                 <h3 className="text-center">
                     Você pode cadastrar o registro do dia e ver o resultado dos ultimos 30 dias
-            </h3>
+                </h3>
                 <div className="d-flex justify-content-center">
                     <div className="row custom-widt mt-5">
                         <div className="col-lg-6">
